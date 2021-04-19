3a9ce544a698562dfd5d0abcb6f2425678ffe973
rezahmady <ahmadireza15@gmail.com>
2021-04-19 22:06:48 +0430

    add https config for websocket

--- config/websockets.php
+++ config/websockets.php
@@ -117,18 +117,20 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', '/www/server/panel/vhost/cert/gariin.com/fullchain.pem'),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', '/www/server/panel/vhost/cert/gariin.com/privkey.pem'),
 
         /*
          * Passphrase for your local_cert file.
          */
         'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
+
+        'verify_peer' => false,
     ],
 
     /*
--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
+    // wssPort: 6001,
     forceTLS: false,
     disableStats: true,
 });
