2f495cf1d42bebf6ae70bcf938d81f984daa2c56
reza ahmadi <ahmadireza15@gmail.com>
2021-07-10 18:34:06 +0430

    urlencode telegram share link

--- resources/views/themes/garrin/partials/blog/share-holder.blade.php
+++ resources/views/themes/garrin/partials/blog/share-holder.blade.php
@@ -11,7 +11,7 @@
     <li><a wire:click="whatsapp()" href="whatsapp://send?text={{$post->whatsappContent}}" data-action="share/whatsapp" title="اشتراک گزاری در واتساپ"><i class="fab fa-whatsapp"></i><span class="social-value">
     {{$whatsapp}}  
     </span></a></li>
-    <li><a wire:click="telegram()" href="https://t.me/iv?url={{ $post->path() }}&rhash=a702ddb0e6dcd" title="اشتراک گزاری در تلگرام"><i class="fab fa-telegram"></i><span class="social-value">
+    <li><a wire:click="telegram()" href="https://t.me/iv?url={{ urlencode($post->path()) }}&rhash=a702ddb0e6dcd" title="اشتراک گزاری در تلگرام"><i class="fab fa-telegram"></i><span class="social-value">
     {{$telegram}}  
     </span></a></li>
 
