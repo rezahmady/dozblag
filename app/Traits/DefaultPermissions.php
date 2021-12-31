8b3d559b511a9f146a1b5b9fca5899fe2e16bde3
Reza Ahmadi Sabzevar <ahmadireza15@gmail.com>
2021-12-31 12:57:59 +0330

    update default permissions

--- app/Traits/DefaultPermissions.php
+++ app/Traits/DefaultPermissions.php
@@ -11,6 +11,8 @@ trait DefaultPermissions
         (backpack_user()->can(self::ENTITY.' create')) ? $this->crud->allowAccess('create') : $this->crud->denyAccess('create'); // add
         (backpack_user()->can(self::ENTITY.' update')) ? $this->crud->allowAccess('update') : $this->crud->denyAccess('update'); // update
         (backpack_user()->can(self::ENTITY.' delete')) ? $this->crud->allowAccess('delete') : $this->crud->denyAccess('delete'); // delete
-        (backpack_user()->can(self::ENTITY.' clone')) ? $this->crud->allowAccess('delete') : $this->crud->denyAccess('clone'); // delete
+        (backpack_user()->can(self::ENTITY.' clone')) ? $this->crud->allowAccess('clone') : $this->crud->denyAccess('clone'); // delete
+        (backpack_user()->can(self::ENTITY.' setting')) ? $this->crud->allowAccess('setting') : $this->crud->denyAccess('setting'); // delete
+        (backpack_user()->can(self::ENTITY.' revise')) ? $this->crud->allowAccess('revise') : $this->crud->denyAccess('revise'); // delete
     }
 }
