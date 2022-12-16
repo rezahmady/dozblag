3eca6a32755684bfa31b564f2853e42dfb4c25fe
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2022-12-17 00:11:02 +0330

    remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
-use App\Http\Controllers\UploadController;
-use Spatie\Sitemap\SitemapIndex;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,21 +12,6 @@ use Spatie\Sitemap\SitemapIndex;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use \Cviebrock\EloquentSluggable\Services\SlugService;
-
-
-Route::get('/admin/generate-sitemap', function() {
-    SitemapIndex::create()
-    ->add('/sitemap/posts_sitemap.xml')
-    ->add('/sitemap/doctors_sitemap.xml')
-    ->add('/sitemap/resources_sitemap.xml')
-    ->add('/sitemap/tags_sitemap.xml')
-    ->add('/sitemap/pages_sitemap.xml')
-    ->writeToFile(public_path('sitemap.xml'));
-});
-
-// At the top of the file.
-Route::post('/admin/upload/file', [UploadController::class, 'file']);
 
 Route::get('/', function(){
     return redirect()->to('/admin');
