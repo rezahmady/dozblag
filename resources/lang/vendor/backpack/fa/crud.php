bede03c44a3f279bfbeb36adbffd168d0ceef21f
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2022-12-19 14:23:05 +0330

    add reset lang

---
+++ resources/lang/vendor/backpack/fa/crud.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack Crud Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the CRUD interface.
+    | You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    // The pivot selector required validation message
+    'reset' => 'تازه سازی صفحه',
+];
