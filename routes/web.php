cabd085e3567974d7aeebbd014035c515d5c10f0
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2023-01-30 00:58:08 +0330

    fix default route redirec

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function(){
-    return redirect()->to(env('ADMIN_ROUTE_PREFIX', 'admin'));
+    return redirect()->to(backpack_url());
 });
 
 Route::post('/admin/api/widget', [ \App\Http\Controllers\Api\DashboardController::class, 'update_widget']);
