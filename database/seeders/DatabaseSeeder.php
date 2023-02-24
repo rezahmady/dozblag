1eddd867479d7c4ab4929b2af1b13c835ce401e9
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2023-02-24 16:37:24 +0330

    fixe install error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,5 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(OstansTableSeeder::class);
         $this->call(ShahrestansTableSeeder::class);
-        $this->call(ModuleSeeder::class);
     }
 }
